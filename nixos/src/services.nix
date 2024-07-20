74a6b63ee61f255d44de744241b3776b7a5260ca
TanawatJukmongkol <tanawat.jukmon@gmail.com>
2024-07-20 07:39:42 +0700

    sddm theme is pain

--- nixos/src/services.nix
+++ nixos/src/services.nix
@@ -23,7 +23,7 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "sddm-theme";
+    theme = "sddm-astronaut-theme";
   };
   services.desktopManager.plasma6.enable = true;
 
--- pkgs/sddm-theme/default.nix
+++ pkgs/sddm-theme/default.nix
@@ -1,16 +1,80 @@
-{ pkgs }:
+{
+  lib,
+  formats,
+  stdenvNoCC,
+  fetchFromGitHub,
+  qt6,
+  libsForQt5,
+  variants ? [ "qt6" ],
+  /*
+    An example of how you can override the background on the NixOS logo
 
-pkgs.stdenv.mkDerivation {
-  name = "sddm-theme";
-  src = pkgs.fetchFromGitHub {
-    owner = "MarianArlt";
-    repo = "sddm-sugar-dark";
-    rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-    sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
+      environment.systemPackages = [
+        (pkgs.where-is-my-sddm-theme.override {
+          themeConfig.General = {
+            background = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            backgroundMode = "none";
+          };
+        })
+      ];
+  */
+  themeConfig ? null,
+}:
+
+let
+  user-cfg = (formats.ini { }).generate "theme.conf.user" themeConfig;
+  validVariants = [
+    "qt5"
+    "qt6"
+  ];
+in
+
+lib.checkListOfEnum "sddm-astronaut-them: variant" validVariants variants
+
+stdenvNoCC.mkDerivation rec {
+  pname = "sddm-astronaut-theme";
+  version = "1.0.1";
+
+  src = fetchFromGitHub {
+    owner = "Keyitdev";
+    repo = pname;
+    rev = "48ea0a792711ac0c58cc74f7a03e2e7ba3dc2ac0";
+    sha256 = "0ga4jvw23vlj52vnfmzvkz16wybn9mrzlfmrnn62y1bprp7jyyli";
   };
-  installPhase = ''
-    mkdir -p $out
-    cp -R ./* $out/
-    cd $out/
-   '';
+
+  propagatedUserEnvPkgs =
+    [ ]
+    ++ lib.optional (lib.elem "qt5" variants) [ libsForQt5.qtgraphicaleffects ]
+    ++ lib.optional (lib.elem "qt6" variants) [
+      qt6.qt5compat
+      qt6.qtsvg
+    ];
+
+  installPhase =
+    ''
+      mkdir -p $out/share/sddm/themes/
+    ''
+    + lib.optionalString (lib.elem "qt6" variants) (
+      ''
+        cp -aR $src $out/share/sddm/themes/$pname
+      ''
+      + lib.optionalString (lib.isAttrs themeConfig) ''
+        ln -sf ${user-cfg} $out/share/sddm/themes/$pname/theme.conf.user
+      ''
+    );
+    # + lib.optionalString (lib.elem "qt5" variants) (
+    #   ''
+    #     cp -r where_is_my_sddm_theme_qt5/ $out/share/sddm/themes/
+    #   ''
+    #   + lib.optionalString (lib.isAttrs themeConfig) ''
+    #     ln -sf ${user-cfg} $out/share/sddm/themes/where_is_my_sddm_theme_qt5/theme.conf.user
+    #   ''
+    # );
+
+  meta = with lib; {
+    description = "Custom SDDM theme";
+    license = licenses.mit;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ name-snrl ];
+  };
 }
